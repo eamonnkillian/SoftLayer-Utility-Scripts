af5a315f7b6a4e3438f85a3dd7d88ffabe16b612
eamonnkillian <eamonnkillian@gmail.com>
2015-08-12 12:50:04 +0100

    Updated the postinstall to correct error

--- sl-postinstall-security.sh
+++ sl-postinstall-security.sh
@@ -70,7 +70,7 @@ mkdir /home/sysadm/.ssh
 # working client machine.
 # 
 
-echo ""ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSiVAJyPr7uDIMLc8FeMdw7aFlShd+lYFLTngXeW5rtnRLLsX80IeeUftwmnf4sz9Yz19vUgCza53obCjIxAtwHM6CZvz3xkivF+lBg8yWto65Yoc+13JDn+ERqzAFx1fdXYtf2ARd8AEWSV2LI5iOFLPrHM6cnGAdk3nkkStlb0okyqBxavx3tzxgEyx9QKvEqB/+qMSj2BV/sRxPbzwCks8O9fjUlHrcEXnSsEb6S4+NP1fzMFR4tiy/wDIQTqCTwfcJ3Rb/3LsdlueIpo34XPjlSFVtQ2g2xe/HW93RcdKH3nX0pAM2O9OTXpmE89xxM/0drV8XgHn20pi6GctV saasify@Eamonns-iMac.local" > /home/admin/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSiVAJyPr7uDIMLc8FeMdw7aFlShd+lYFLTngXeW5rtnRLLsX80IeeUftwmnf4sz9Yz19vUgCza53obCjIxAtwHM6CZvz3xkivF+lBg8yWto65Yoc+13JDn+ERqzAFx1fdXYtf2ARd8AEWSV2LI5iOFLPrHM6cnGAdk3nkkStlb0okyqBxavx3tzxgEyx9QKvEqB/+qMSj2BV/sRxPbzwCks8O9fjUlHrcEXnSsEb6S4+NP1fzMFR4tiy/wDIQTqCTwfcJ3Rb/3LsdlueIpo34XPjlSFVtQ2g2xe/HW93RcdKH3nX0pAM2O9OTXpmE89xxM/0drV8XgHn20pi6GctV saasify@Eamonns-iMac.local" > /home/sysadm/.ssh/authorized_keys
 
 # 
 # Now we need to make sure of the permissions and ownership of our ssh 
